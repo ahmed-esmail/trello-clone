46bc7143460161e1ebb8523d26262ddaad654227
ahmed.esmail <ahmed.esmail.gm@gmail.com>
2023-12-26 23:21:42 +0200

    add array helper function

--- src/utils/arrayUtil.tsx
+++ src/utils/arrayUtil.tsx
@@ -2,9 +2,34 @@ type Item = {
   id: string;
 };
 
-export const findItemIndexById = <TItem extends Item>(
+export function findItemIndexById<TItem extends Item>(
   items: TItem[],
   id: string,
-) => {
+) {
   return items.findIndex((i) => i.id === id);
-};
+}
+
+export function removeItemAtIndex<TItem extends Item>(
+  items: TItem[],
+  index: number,
+) {
+  return [...items.slice(0, index), ...items.slice(index + 1)];
+}
+
+export function insertItemAtIndex<TItem extends Item>(
+  items: TItem[],
+  index: number,
+  item: TItem,
+) {
+  return [...items.slice(0, index), item, ...items.slice(index)];
+}
+
+export function moveItem<TItem extends Item>(
+  items: TItem[],
+  from: number,
+  to: number,
+) {
+  const item = items[from];
+  const newItems = removeItemAtIndex(items, from);
+  return insertItemAtIndex(newItems, to, item);
+}
